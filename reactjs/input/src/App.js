08dbc5ea2f83723b17bca83a18a8eb743e0e70b5
hikmahgumelar <hikmahgumelar@gmail.com>
2018-04-10 17:49:59 +0700

    TensorflowJS

--- reactjs/input/src/App.js
+++ reactjs/input/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './form.js';
-
+import { TfImageRecognition } from 'react-native-tensorflow';
 
 
 class App extends Component {
 
    constructor() {
     super();
-    this.state = {
-      
-      statusNow :'',
-    
-    }
-  
+    this.image = require('./assets/dumbbell.jpg');
+    this.state = {result:''}
   }
+   componentDidMount(){
+    this.recognizeImage()
+   }
+
+   async recognizeImage() {
+   try {
+    const tfImageRecognition = new TfImageRecognition({
+      model: require('./assets/tensorflow_inception_graph.pb'),
+      labels: require('./assets/tensorflow_labels.txt')
+    
+    })
+   const results = await tfImageRecognition.recognize({
+    image: this.image
+   })
+   const resultText = `Name: ${results[0].name} - Confidence: ${results[0].confidence}`
+     this.setState({result: resultText})
+
+     await tfImageRecognition.close()
+   } catch(err) {
+     alert(err)
+   }
+}
   render() {
     return (
       <div className="App">
@@ -23,10 +40,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <img src={this.image} alt='test bray'/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        Gambar di atas adalah :
+        <h3>{this.state.result}</h3>
         </p>
-        <Form />
       </div>
     );
   }
